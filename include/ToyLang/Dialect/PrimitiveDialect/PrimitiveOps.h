a1d84c5a1c95625756bc4bb42c4358716538352d
kfirdev <kfir21022006@gmail.com>
2025-01-22 10:29:54 +0200

    Relates to #4 #7: Add int add op and test for it

---
+++ include/ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "mlir/IR/DialectImplementation.h"
+
+#define GET_OP_CLASSES
+#include "ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.h.inc"
--- lib/Dialect/PrimitiveDialect/PrimitiveDialect.cpp
+++ lib/Dialect/PrimitiveDialect/PrimitiveDialect.cpp
@@ -1,12 +1,16 @@
 #include "include/ToyLang/Dialect/PrimitiveDialect/PrimitiveDialect.h"
 
 #include "include/ToyLang/Dialect/PrimitiveDialect/PrimitiveTypes.h"
+#include "include/ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.h"
 #include "mlir/IR/Builders.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 #include "ToyLang/Dialect/PrimitiveDialect/PrimitiveDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "ToyLang/Dialect/PrimitiveDialect/PrimitiveTypes.cpp.inc"
+
+#define GET_OP_CLASSES
+#include "ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.cpp.inc"
 
 
 namespace mlir::toylang::primitive{
@@ -15,6 +19,11 @@ void PrimitiveDialect::initialize(){
 	addTypes<
 #define GET_TYPEDEF_LIST 
 #include "ToyLang/Dialect/PrimitiveDialect/PrimitiveTypes.cpp.inc"
+		>();
+
+	addOperations<
+	#define GET_OP_LIST
+	#include "ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.cpp.inc"
 		>();
 }
 
