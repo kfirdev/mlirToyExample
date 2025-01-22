db949d85e8a80d3bd65aede55e782fe75004dcd8
kfirdev <kfir21022006@gmail.com>
2025-01-22 11:26:28 +0200

    Relates to #4 #7: Add traits

--- include/ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.h
+++ include/ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#include "mlir/IR/DialectImplementation.h"
+//#include "mlir/IR/DialectImplementation.h"
+#include "include/ToyLang/Dialect/PrimitiveDialect/PrimitiveDialect.h"
+#include "include/ToyLang/Dialect/PrimitiveDialect/PrimitiveTypes.h"
+#include "mlir/IR/BuiltinOps.h"    // from @llvm-project
+#include "mlir/IR/BuiltinTypes.h"  // from @llvm-project
+#include "mlir/IR/Dialect.h"
 
 #define GET_OP_CLASSES
 #include "ToyLang/Dialect/PrimitiveDialect/PrimitiveOps.h.inc"
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "include/ToyLang/Dialect/PrimitiveDialect/PrimitiveDialect.h"
 #include "mlir/InitAllDialects.h"
+//#include "mlir/InitAllPasses.h"
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+//#include "mlir/Transforms/Passes.h"
 
 int main(int argc, char **argv) {
 	mlir::DialectRegistry registry;
 	registry.insert<mlir::toylang::primitive::PrimitiveDialect>();
 	mlir::registerAllDialects(registry);
+	//mlir::registerAllPasses();
 
 	return mlir::asMainReturnCode(
   	    mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
